f43708eea1a52dc93f0f29ee2ad5571d73182232
ParkMinUk <parkmxxuk@gmail.com>
2023-06-14 23:58:27 +0900

    객체 인식 모형 동그라미 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import required packages
 var AWS = require('aws-sdk');
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const fs = require('fs');
 
 // AWS access details
 AWS.config.update({
@@ -31,10 +33,6 @@ rekognition.detectLabels(params, function(err, data) {
         console.log(data); // successful response
         const labels = data.Labels; // Assign the detected labels to the 'labels' array
 
-        ///////////////////////////////////
-        const { createCanvas, loadImage, registerFont } = require('canvas');
-        const fs = require('fs');
-
         // Load Arial font
         registerFont('arial.ttf', { family: 'Sans' });
 
@@ -58,11 +56,8 @@ rekognition.detectLabels(params, function(err, data) {
             const fontSize = 40;
             const fontFamily = 'Arial';
             context.font = `${fontSize}px ${fontFamily}`;
-            context.lineWidth = 10;
-            context.fillStyle = 'red';
-            context.strokeStyle = 'red';
 
-            // Draw labels on the image
+            // Draw circles and rectangles around objects in the foreground
             labels.forEach((label) => {
                 const name = label.Name;
                 label.Instances.forEach((instance) => {
@@ -75,11 +70,29 @@ rekognition.detectLabels(params, function(err, data) {
                         const width = Width * imageWidth;
                         const height = Height * imageHeight;
 
-                        // Draw the bounding box and label text
+                        // Calculate the center coordinates of the bounding box
+                        const centerX = x + width / 2;
+                        const centerY = y + height / 2;
+
+                        // Draw the circle
+                        context.beginPath();
+                        context.arc(centerX, centerY, Math.max(width, height) / 2, 0, 2 * Math.PI);
+                        context.lineWidth = 5;
+                        context.strokeStyle = 'blue';
+                        context.fillStyle = 'transparent';
+                        context.stroke();
+
+                        // Draw the rectangle
                         context.beginPath();
                         context.rect(x, y, width, height);
+                        context.lineWidth = 5;
+                        context.strokeStyle = 'red';
+                        context.fillStyle = 'transparent';
                         context.stroke();
-                        context.fillText(name, x, y - 5); // Adjust the text position to be above the bounding box
+
+                        // Draw the label text
+                        context.fillStyle = 'white'; // Set the text color to white
+                        context.fillText(name, x, y - 5); // Adjust the text position to be above the shapes
                     }
                 });
             });
@@ -92,6 +105,129 @@ rekognition.detectLabels(params, function(err, data) {
                 console.log('Image saved: labeled_image4.jpg');
             });
         });
-        ///////////////////////////////////
     }
 });
+
+
+// // Import required packages
+// var AWS = require('aws-sdk');
+// const { createCanvas, loadImage, registerFont } = require('canvas');
+// const fs = require('fs');
+
+// // AWS access details
+// AWS.config.update({
+//     accessKeyId: 'AKIAZWBYDAILKLJD4LMZ',
+//     secretAccessKey: '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',
+//     region: 'ap-northeast-2'
+// });
+
+
+// // Input parameters
+// var params = {
+//     Image: {
+//         S3Object: {
+//             Bucket: 'projecttest2-1',
+//             Name: 'project_sample6.jpg'
+//         }
+//     },
+//     MaxLabels: 5,
+//     MinConfidence: 80
+// };
+
+// // Call AWS Rekognition Class
+// const rekognition = new AWS.Rekognition();
+
+// // Detect labels
+// rekognition.detectLabels(params, function(err, data) {
+//     if (err) {
+//         console.log(err, err.stack); // an error occurred
+//     } else {
+//         console.log(data); // successful response
+//         const labels = data.Labels; // Assign the detected labels to the 'labels' array
+
+//         // Load Arial font
+//         registerFont('arial.ttf', { family: 'Sans' });
+
+//         // Create a canvas
+//         const canvas = createCanvas();
+//         const context = canvas.getContext('2d');
+
+//         // Load and draw the image
+//         loadImage('project_sample6.jpg').then((image) => {
+//             // Get the original image dimensions
+//             const imageWidth = image.width;
+//             const imageHeight = image.height;
+
+//             // Resize the canvas to match the original image size
+//             canvas.width = imageWidth;
+//             canvas.height = imageHeight;
+
+//             context.drawImage(image, 0, 0);
+
+//             // Set font properties
+//             const fontSize = 40;
+//             const fontFamily = 'Arial';
+//             context.font = `${fontSize}px ${fontFamily}`;
+
+//             // Draw circles and rectangles around objects
+//             labels.forEach((label) => {
+//                 const name = label.Name;
+//                 label.Instances.forEach((instance, index) => {
+//                     if (instance.BoundingBox && instance.BoundingBox.Left) {
+//                         const { Left, Top, Width, Height } = instance.BoundingBox;
+
+//                         // Calculate the bounding box coordinates relative to the original image size
+//                         const x = Left * imageWidth;
+//                         const y = Top * imageHeight;
+//                         const width = Width * imageWidth;
+//                         const height = Height * imageHeight;
+
+//                         // Draw the shape based on the object's index
+//                         if (index === 0) {
+//                             // Draw a circle
+//                             const centerX = x + width / 2;
+//                             const centerY = y + height / 2;
+//                             const radius = Math.max(width, height) / 2;
+
+//                             context.beginPath();
+//                             context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+//                             context.lineWidth = 5;
+//                             context.strokeStyle = 'blue';
+//                             context.fillStyle = 'transparent';
+//                             context.stroke();
+
+//                             // Draw a rectangle
+//                             context.beginPath();
+//                             context.rect(x, y, width, height);
+//                             context.lineWidth = 5;
+//                             context.strokeStyle = 'red';
+//                             context.fillStyle = 'transparent';
+//                             context.stroke();
+
+//                         } else {
+//                             // Draw a rectangle
+//                             context.beginPath();
+//                             context.rect(x, y, width, height);
+//                             context.lineWidth = 5;
+//                             context.strokeStyle = 'red';
+//                             context.fillStyle = 'transparent';
+//                             context.stroke();
+//                         }
+
+//                         // Draw the label text
+//                         context.fillStyle = 'white';
+//                         context.fillText(name, x, y - 5);
+//                     }
+//                 });
+//             });
+
+//             // Save the image
+//             const out = fs.createWriteStream('labeled_image6.jpg');
+//             const stream = canvas.createJPEGStream();
+//             stream.pipe(out);
+//             out.on('finish', () => {
+//                 console.log('Image saved: labeled_image6.jpg');
+//             });
+//         });
+//     }
+// });
